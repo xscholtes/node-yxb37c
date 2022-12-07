1ff2c7a5e6e4267b1405534885307be4de430975
xscholtes <xavier.scholtes@gmail.com>
2022-12-07 13:24:36 +0100

    2 files modified

--- day4.js
+++ day4.js
@@ -1,50 +1,41 @@
-import { open } from 'fs/promises';
-import { uniq, range, difference } from 'underscore';
-let filehandle;
-try {
-  function calculate(data) {
-    //result A
-    var pairs = data
-      .toString()
-      .split('\r\n')
-      .map((a) => {
-        var slots = a
-          .split(',')
-          .map((s) =>
-            range(parseInt(s.split('-')[0]), parseInt(s.split('-')[1]) + 1)
-          );
-        return difference(slots[1], slots[0]).length == 0 ||
-          difference(slots[0], slots[1]).length == 0
-          ? 1
-          : 0;
-      });
-    console.log(pairs.reduce((a, b) => a + b, 0));
-    //result B
-    var pairsOverlap = data
-      .toString()
-      .split('\r\n')
-      .map((a) => {
-        var slots = a
-          .split(',')
-          .map((s) =>
-            range(parseInt(s.split('-')[0]), parseInt(s.split('-')[1]) + 1)
-          );
-        return difference(slots[0], slots[1]).length < slots[0].length ||
-          difference(slots[1], slots[0]).length < slots[1].length
-          ? 1
-          : 0;
-      });
-    console.log(pairsOverlap.reduce((a, b) => a + b, 0));
-  }
+import { rf } from './utils/rf.js';
+import { range, difference } from 'underscore';
 
-  let filehandle = await open('day4_sample.txt', 'r');
-  console.log(`sample 4`);
-  let reader = filehandle.createReadStream();
-  reader.on('data', calculate);
-  filehandle = await open('day4_input.txt', 'r');
-  console.log(`input 4`);
-  reader = filehandle.createReadStream();
-  reader.on('data', calculate);
-} finally {
-  await filehandle?.close();
+function calculate(data) {
+  //result A
+  var pairs = data
+    .toString()
+    .split('\r\n')
+    .map((a) => {
+      var slots = a
+        .split(',')
+        .map((s) =>
+          range(parseInt(s.split('-')[0]), parseInt(s.split('-')[1]) + 1)
+        );
+      return difference(slots[1], slots[0]).length == 0 ||
+        difference(slots[0], slots[1]).length == 0
+        ? 1
+        : 0;
+    });
+  var result1 = pairs.reduce((a, b) => a + b, 0);
+  //result B
+  var pairsOverlap = data
+    .toString()
+    .split('\r\n')
+    .map((a) => {
+      var slots = a
+        .split(',')
+        .map((s) =>
+          range(parseInt(s.split('-')[0]), parseInt(s.split('-')[1]) + 1)
+        );
+      return difference(slots[0], slots[1]).length < slots[0].length ||
+        difference(slots[1], slots[0]).length < slots[1].length
+        ? 1
+        : 0;
+    });
+  var result2 = pairsOverlap.reduce((a, b) => a + b, 0);
+  return [result1, result2];
 }
+
+rf('day4/sample.txt', calculate);
+rf('day4/input.txt', calculate);
