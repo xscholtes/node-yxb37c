f9be6a734f2ad0d25ff677e357414898d502704c
xscholtes <xavier.scholtes@gmail.com>
2022-12-08 14:46:31 +0100

    Update day8.js

--- day8.js
+++ day8.js
@@ -1,5 +1,5 @@
 import { rf } from './utils/rf.js';
-
+import { findIndex, last, findLastIndex, first } from 'underscore';
 //https://adventofcode.com/2022/day/8
 
 function calculate(data) {
@@ -11,12 +11,15 @@ function calculate(data) {
   let width = input[0].length - 1;
   let height = input.length - 1;
   let grid = [];
+  let grid2 = [];
   let bm = [],
     tp = [];
   for (let i = 0; i <= height; ++i) {
     grid[i] = [];
+    grid2[i] = [];
     for (let j = 0; j <= width; ++j) {
       grid[i][j] = 0;
+      grid2[i][j] = 1;
       bm[j] = 0;
       tp[j] = 0;
     }
@@ -49,13 +52,74 @@ function calculate(data) {
         i == 0 || input[height - i][j] >= bm[j] + 1
           ? input[height - i][j]
           : bm[j];
+
+      //SOLUTION B
+      let idx = findIndex(last(input[i], width - j), (a) => a >= input[i][j]);
+      let visible = idx == -1 ? width - j : idx + 1;
+      if (i == 3 && j == 2) {
+        console.log('LEFT', visible);
+      }
+      grid2[i][j] = visible != 0 ? grid2[i][j] * visible : grid2[i][j];
+
+      idx = findIndex(
+        first(input[i], width - j).reverse(),
+        (a) => a >= input[i][width - j]
+      );
+      visible = idx == -1 ? width - j : idx + 1;
+      if (i == 3 && j == 2) {
+        console.log('RIGHT', visible);
+      }
+      grid2[i][width - j] =
+        visible != 0 ? grid2[i][width - j] * visible : grid2[i][width - j];
+
+      idx = findIndex(
+        last(
+          input.map((line) => line[j]),
+          height - i
+        ),
+        (a) => a >= input[i][j]
+      );
+      visible = idx == -1 ? height - i : idx + 1;
+      if (i == 3 && j == 2) {
+        console.log('DOWN', visible);
+      }
+      grid2[i][j] = visible != 0 ? grid2[i][j] * visible : grid2[i][j];
+
+      idx = findIndex(
+        last(
+          input.map((line) => line[j]),
+          height - i
+        ).reverse(),
+        (a) => a >= input[height - i][j]
+      );
+      visible = idx == -1 ? height - i : idx + 1;
+      if (i == 3 && j == 2) {
+        console.log(
+          'UP',
+          visible,
+          last(
+            input.map((line) => line[j]),
+            i
+          ).reverse()
+        );
+      }
+      grid2[height - i][j] =
+        visible != 0 ? grid2[height - i][j] * visible : grid2[height - i][j];
     }
   }
-  console.log(grid);
+  console.log(input);
+  console.log(grid2);
   let result1 = grid
     .map((a, b) => a.reduce((x, y) => (x += y >= 1 ? 1 : 0), 0))
     .reduce((a, b) => a + b, 0);
-  return [result1, 'No'];
+  let result2 = grid2
+    .map((a, x) =>
+      a.map((b, y) => (x == 0 || y == 0 || x == height || y == width ? 0 : b))
+    )
+    .map((x) => x.reduce((a, b) => (a >= b ? a : b), 0))
+    .reduce((a, b) => (a >= b ? a : b), 0);
+  //1006740
+  return [result1, result2];
 }
-rf('day8/sample.txt', calculate, [21, 0]);
+rf('day8/sample.txt', calculate, [21, 8]);
 //rf('day8/input.txt', calculate);
