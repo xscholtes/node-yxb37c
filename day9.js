e0bae83cecce02ba9b58d312b626294b2ba21b04
Xavier Scholtes <xavier.scholtes@gmail.com>
2022-12-09 15:21:28 +0100

    day9

--- day9.js
+++ day9.js
@@ -8,49 +8,29 @@ function calculate(data) {
     .map((a) => [a.split(' ')[0], parseInt(a.split(' ')[1])]);
   function calculateMovesPositions(moves) {
     const tensors = [
-      [
-        [0, 0],
-        [-1, -1],
-        [-1, 0],
-        [-1, 1],
-        [0, 0],
-      ],
-      [
-        [-1, -1],
-        [0, 0],
-        [0, 0],
-        [0, 0],
-        [-1, 1],
-      ],
-      [
-        [0, -1],
-        [0, 0],
-        [0, 0],
-        [0, 0],
-        [0, 1],
-      ],
-      [
-        [1, -1],
-        [0, 0],
-        [0, 0],
-        [0, 0],
-        [1, 1],
-      ],
-      [
-        [0, 0],
-        [1, -1],
-        [1, 0],
-        [1, 1],
-        [0, 0],
-      ],
+      [[-1, -1], [-1, -1], [-1, 0], [-1, 1], [-1, 1],],
+      [[-1, -1], [0, 0], [0, 0], [0, 0], [-1, 1],],
+      [[0, -1], [0, 0], [0, 0], [0, 0], [0, 1],],
+      [[1, -1], [0, 0], [0, 0], [0, 0], [1, 1],],
+      [[1, -1], [1, -1], [1, 0], [1, 1], [1, 1],],
     ];
     let t = [0, 0];
+    let t2 = [0, 0];
+    let t3 = [0, 0];
+    let t4 = [0, 0];
+    let t5 = [0, 0];
+    let t6 = [0, 0];
+    let t7 = [0, 0];
+    let t8 = [0, 0];
+    let t9 = [0, 0];
     let h = [0, 0];
     let di = 0;
     let dj = 0;
     let pos = [];
+    let pos9 = [];
     let tmoves = [];
     pos.push(t);
+    pos9.push(t9);
     moves.forEach((m) => {
       dj = m[0] == 'R' ? 1 : m[0] == 'L' ? -1 : 0;
       di = m[0] == 'D' ? 1 : m[0] == 'U' ? -1 : 0;
@@ -58,48 +38,47 @@ function calculate(data) {
         h[0] += di;
         h[1] += dj;
         let tensor = tensors[h[0] - t[0] + 2][h[1] - t[1] + 2];
-        if (tensor[0] !== 0) {
-          tmoves.push(tensor[0] == 1 ? ['D', 1] : ['U', 1]);
-        }
-        if (tensor[1] !== 0) {
-          tmoves.push(tensor[1] == 1 ? ['R', 1] : ['L', 1]);
-        }
         t = [t[0] + tensor[0], t[1] + tensor[1]];
+
+        let tensor2 = tensors[t[0] - t2[0] + 2][t[1] - t2[1] + 2];
+        t2 = [t2[0] + tensor2[0], t2[1] + tensor2[1]];
+
+        let tensor3 = tensors[t2[0] - t3[0] + 2][t2[1] - t3[1] + 2];
+        t3 = [t3[0] + tensor3[0], t3[1] + tensor3[1]];
+
+        let tensor4 = tensors[t3[0] - t4[0] + 2][t3[1] - t4[1] + 2];
+        t4= [t4[0] + tensor4[0], t4[1] + tensor4[1]];
+        
+        let tensor5 = tensors[t4[0] - t5[0] + 2][t4[1] - t5[1] + 2];
+        t5 = [t5[0] + tensor5[0], t5[1] + tensor5[1]];
+        
+        let tensor6 = tensors[t5[0] - t6[0] + 2][t5[1] - t6[1] + 2];
+        t6 = [t6[0] + tensor6[0], t6[1] + tensor6[1]];
+        
+        let tensor7 = tensors[t6[0] - t7[0] + 2][t6[1] - t7[1] + 2];
+        t7 = [t7[0] + tensor7[0], t7[1] + tensor7[1]];
+        
+        let tensor8 = tensors[t7[0] - t8[0] + 2][t7[1] - t8[1] + 2];
+        t8 = [t8[0] + tensor8[0], t8[1] + tensor8[1]];
+        
+        let tensor9 = tensors[t8[0] - t9[0] + 2][t8[1] - t9[1] + 2];
+        t9 = [t9[0] + tensor9[0], t9[1] + tensor9[1]];
+        
         if (pos.filter((p) => p[0] == t[0] && p[1] == t[1]) == 0) {
           pos.push(t);
+        }
+        if (pos9.filter((p) => p[0] == t9[0] && p[1] == t9[1]) == 0) {
+          pos9.push(t9);
         }
       }
     });
     let r = [];
-    tmoves.forEach((a) =>
-      r.length != 0 && r[r.length - 1][0] == a[0]
-        ? (r[r.length - 1][1] += a[1])
-        : r.push(a)
-    );
-    return [pos, r];
+    
+    return [pos, pos9];
   }
   let result1 = calculateMovesPositions(moves);
-  console.log(render(result1[0], '#', 16));
-  let result2 = result1;
-  for (let m = 2; m <= 9; ++m) {
-    result2 = calculateMovesPositions(result2[1]);
-  }
-  console.log(render(result2[0], '#', parseInt(25)));
-  function render(positions, symbol, size) {
-    let map = Array(size * 2)
-      .fill('')
-      .map((a) => Array(size * 2).fill('.'));
-    let temp = positions.map((a) => [a[0] + size, a[1] + size]);
-    temp.forEach((p) => (map[p[0]][p[1]] = symbol));
-    var tx = '';
-    map.forEach((a) => {
-      a.forEach((x) => (tx += x));
-      tx += '\n';
-    });
-    return tx;
-  }
-  return [result1[0].length, result2[0].length];
+  return [result1[0].length, result1[1].length];
 }
 rf('day9/sample.txt', calculate, [13, 1]);
 rf('day9/sample2.txt', calculate, [88, 36]);
-//rf('day9/input.txt', calculate);
+rf('day9/input.txt', calculate);
