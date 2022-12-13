7da60b22c89d5ca222c0eb86eaf08950b2dca1f2
Xavier Scholtes <xavier.scholtes@gmail.com>
2022-12-13 09:23:36 +0100

    data

--- 2022/day13.js
+++ 2022/day13.js
@@ -2,9 +2,13 @@ import { rf } from './../utils/rf.js';
 
 function calculate(data) {
   //PARSING
-  var input = data.toString().split('\n');
-
-  return [input[0], input.length];
+  var pairs = data.toString().split('\n\n').map( p => p.split('\n').map(a => eval(a)));
+  pairs.forEach(p => {
+    var left = p[0]
+    console.log(p[0],p[1]);
+  });
+  console.log(pairs);
+  return [pairs.length, pairs.length];
 }
 rf('2022/day13/sample.txt', calculate,['sample input',1]);
-rf('2022/day13/input.txt', calculate);
+//rf('2022/day13/input.txt', calculate);
